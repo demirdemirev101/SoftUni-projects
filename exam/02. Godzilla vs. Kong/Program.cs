73bfd858dde0c7608bb7dc5858895e1b49feb761
Demir Demirev <demirdemirev101@gmail.com>
2022-05-25 16:37:38 +0300

    Exercises

---
+++ exam/02. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02._Godzilla_vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double budget = double.Parse(Console.ReadLine());
+            int numStatisticks = int.Parse(Console.ReadLine());
+            double priceForStatisticks = double.Parse(Console.ReadLine());
+
+
+            double price = priceForStatisticks * numStatisticks;
+            double decor = budget * 0.1;
+
+            if (numStatisticks >= 150)
+            {
+                price -= price * 0.1;
+            }
+
+            double sumForFilm = decor + price;
+
+
+            if (budget >= sumForFilm)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget - sumForFilm:f2} leva left.");
+            }
+            else
+            {
+
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {Math.Abs(budget - sumForFilm):f2} leva more.");
+            }
+        }
+    }
+}
---
+++ exam/03. Oscars week in cinema/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _03._Oscars_week_in_cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine();
+            string hallType = Console.ReadLine();
+            int tickets = int.Parse(Console.ReadLine());
+            double price = 0;
+            switch (movie)
+            {
+                case "A Star Is Born":
+                    if (hallType=="normal")
+                    {
+                        price = 7.50;
+                    }
+                    else if (hallType=="luxury")
+                    {
+                        price = 10.50;
+                    }
+                    else if (hallType=="ultra luxury")
+                    {
+                        price = 13.50;
+                    }
+                    break;
+                case "Bohemian Rhapsody":
+                    if (hallType == "normal")
+                    {
+                        price = 7.35;
+                    }
+                    else if (hallType == "luxury")
+                    {
+                        price = 9.45;
+                    }
+                    else if (hallType == "ultra luxury")
+                    {
+                        price = 12.75;
+                    }
+                    break;
+                case "Green Book":
+                    if (hallType == "normal")
+                    {
+                        price = 8.15;
+                    }
+                    else if (hallType == "luxury")
+                    {
+                        price = 10.25;
+                    }
+                    else if (hallType == "ultra luxury")
+                    {
+                        price = 13.25;
+                    }
+                    break;
+                case "The Favourite":
+                    if (hallType == "normal")
+                    {
+                        price = 8.75;
+                    }
+                    else if (hallType == "luxury")
+                    {
+                        price = 11.55;
+                    }
+                    else if (hallType == "ultra luxury")
+                    {
+                        price = 13.95;
+                    }
+                    break;
+                
+                   
+            }
+            price = tickets * price;
+            Console.WriteLine($"{movie} -> {price:f2} lv.");
+        }
+    }
+}
