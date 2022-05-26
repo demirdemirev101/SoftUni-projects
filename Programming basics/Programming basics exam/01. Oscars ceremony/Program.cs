baad7e62e61163eef0dd910e3e7a707912d255e4
Demir Demirev <demirdemirev101@gmail.com>
2022-05-26 11:44:20 +0300

    Programming basics exercises and exam.

---
+++ Programming basics/Programming basics exam/01. Oscars ceremony/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._Oscars_ceremony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rentForHall = int.Parse(Console.ReadLine());
+            double statues = rentForHall - rentForHall*0.3;
+            double catering  = statues - statues*0.15;
+            double soundSystem = catering * 1 / 2;
+            double sum = rentForHall + statues + catering + soundSystem;
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
