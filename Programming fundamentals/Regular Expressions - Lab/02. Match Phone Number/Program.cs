1949cbad33b68c01a9e762601e50bd4fd52e29ef
Demir <demirdemirev101@gmail.com>
2022-09-04 16:22:51 +0300

    Regular Expressions - Lab

---
+++ Programming fundamentals/Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string phoneNumbers=Console.ReadLine();
+            string pattern = @"\+359(-? ?)2(-? ?)[0-9]{3}(-? ?)[0-9]{4}\b";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches=regex.Matches(phoneNumbers);
+            var matchedPhones = matches.Cast<Match>()
+                .Select(m => m.Value.Trim())
+                .ToArray();
+            Console.WriteLine(string.Join(", ",matchedPhones));
+        }
+    }
+}
---
+++ Programming fundamentals/Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            string dates=Console.ReadLine();
+            string pattern = @"(?<day>[0-9]{2})(?<separator>[\.\-\/])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>[0-9]{4})";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches=regex.Matches(dates);
+
+            foreach (Match match in matches)
+            {
+                var day=match.Groups["day"].Value;
+                var month=match.Groups["month"].Value;
+                var year=match.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
