741c8db7782e085040441cd32ee41e133df6ee92
Demir <demirdemirev101@gmail.com>
2022-09-04 16:25:15 +0300

    Regular Expressions - Exercise

---
+++ Programming fundamentals/Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @">>(?<name>[A-Z]+?[a-z]*?)<<(?<price>\d+(\.?\d+)?)!(?<quantity>\d*)";
+            List<string> list = new List<string>(); 
+            double totalPrice = 0;
+            while (input != "Purchase")
+            {
+                Regex regex = new Regex(pattern);
+               
+                Match match = regex.Match(input);
+                if (match.Success)
+                {
+                    var name=match.Groups["name"].Value;
+                    var price = double.Parse(match.Groups["price"].Value);
+                    var quantity = int.Parse(match.Groups["quantity"].Value);
+                    list.Add(name);
+                    totalPrice+=price*quantity;
+                }
+                
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Bought furniture:");
+            list.ForEach(Console.WriteLine);
+            Console.WriteLine($"Total money spend: {totalPrice:f2}");
+
+        }
+    }
+}
---
+++ Programming fundamentals/Regular Expressions - Exercise/02. Race/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names=Console.ReadLine().Split().ToList();
+            string patternName= @"(?<name>[A-Za-z])";
+            string patternDistance= @"(?<distance>\d+)";
+
+            List<string > list=new List<string>();  
+
+            string input=Console.ReadLine();
+            while (input!="end of race")
+            {
+                Regex regexName=new Regex(patternName);
+                Regex regexDistance=new Regex(patternDistance);
+                
+                Match matchNames=regexName.Match(input);
+                Match matchDistance=regexDistance.Match(input);
+                if (matchNames.Success && matchDistance.Success)
+                {
+                    if (names.Contains(matchNames.Groups["name"].Value))
+                    {
+                        var name = matchNames.Groups["name"].Value;
+                        var distance = int.Parse(matchDistance.Groups["distance"].Value);
+                        distance += distance;
+                        list.Add(name);
+                    }
+                }
+                input=Console.ReadLine();
+            }
+        }
+    }
+}
