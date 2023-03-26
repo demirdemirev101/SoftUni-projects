2290a9e9aa33c286a18760be8e225891721c3ade
Demir <demirdemirev101@gmail.com>
2023-03-26 12:32:33 +0300

    InterfacesAndAbstraction - Lab

---
+++ C# OOP/InterfacesAndAbstraction - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int height; 
+        private int width;
+        public Rectangle(int height,int  width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+            for (int i = 1; i < this.height - 1; ++i)
+                DrawLine(this.width, '*', ' ');
+            DrawLine(this.width, '*', '*');
+        }
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width - 1; ++i)
+                Console.Write(mid);
+            Console.WriteLine(end);
+        }
+
+    }
+}
