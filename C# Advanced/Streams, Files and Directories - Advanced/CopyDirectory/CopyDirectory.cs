73fc9ff198098c0baa7e04022cc063f6b3c6a6c7
Demir <demirdemirev101@gmail.com>
2023-03-26 11:46:12 +0300

    Streams, Files and Directories - Advanced

---
+++ C# Advanced/Streams, Files and Directories - Advanced/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,36 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            // string inputPath =  @$"{Console.ReadLine()}";
+            string inputPath = @$"D:\Papka 1";
+            //string outputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"D:\Papka 2";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath);
+            }
+            Directory.CreateDirectory(outputPath);
+            string[] files = Directory.GetDirectories(inputPath);
+            foreach (var file in files)
+            {
+                string fileName = Path.GetFileName(file);
+
+                string destination = Path.Combine(outputPath, fileName);
+                File.Copy(file, destination);
+            }
+        }
+    }
+}
+
---
+++ C# Advanced/Streams, Files and Directories - Advanced/EvenLines/EvenLines.cs
@@ -0,0 +1,58 @@
+﻿
+namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    public class EvenLines
+    {
+
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StreamReader streamReader = new StreamReader(inputFilePath);
+            StringBuilder sb = new StringBuilder();
+           
+            
+                int count = 0;
+                string line = "";
+                while (line != null)
+                {
+                    line = streamReader.ReadLine();
+                    if (count % 2 == 0)
+                    {
+
+                    string[] symbolsToReplace = { "-", ",", ".", "!", "?" };
+
+                    foreach (var symbol in symbolsToReplace)
+                    {
+                        line = line.Replace(symbol, "@");
+                    }
+                    //string replace = SymbolsToReplace(line).ToString();
+
+                    
+                    string[] reverse = line.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                             .Reverse()
+                             .ToArray();
+
+                    string reverseToString = string.Join(" ", reverse);
+
+                    sb.AppendLine(reverseToString);
+
+                    }
+                    count++;
+
+                }
+            
+            return sb.ToString().TrimEnd();
+        }
+       
+    }
+}
