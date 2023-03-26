e345c2167ce3cbb40c0bcf765133bbe5c63f6b57
Demir <demirdemirev101@gmail.com>
2023-03-26 12:03:15 +0300

    Inheritance - Lab

---
+++ C# OOP/Inheritance - Lab/CustomRandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList random = new RandomList();
+            random.Add("1");
+            random.Add("2");
+            random.Add("3");
+            random.Add("4");
+            Console.WriteLine(random.RandomString());
+
+        }
+    }
+}
---
+++ C# OOP/Inheritance - Lab/CustomStack/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+          StackOfStrings customStack=new StackOfStrings();
+            customStack.AddRange(new List<string>() { "1","2","3","4" });
+            customStack.AddRange(new List<string>() { "5","6","7","8" });
+            while (!customStack.IsEmpty())
+            {
+                Console.WriteLine(customStack.Pop());
+            }
+
+        }
+    }
+}
