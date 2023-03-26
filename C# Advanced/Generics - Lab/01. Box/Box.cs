a78cf72ac3514b78cb4632d80b3eb8eee02b2b86
Demir <demirdemirev101@gmail.com>
2023-03-26 11:55:17 +0300

    Generics - Lab

---
+++ C# Advanced/Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace _01._Box
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            list = new List<T>();
+        }
+        private List<T> list;
+        public int Count { get { return list.Count; } }
+        public void Add(T element)
+        {
+            list.Insert(0, element);
+        }
+        public T Remove()
+        {
+            var lastElement = list[0];
+            list.Remove(list[0]);
+            return lastElement;
+        }
+    }
+}
---
+++ C# Advanced/Generics - Lab/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = ArrayCreator.Create(3, 123);
+
+            string[] names = ArrayCreator.Create(3, "Demir");
+
+            Console.WriteLine(string.Join(", ",numbers));
+
+            Console.WriteLine(string.Join(", ",names));
+        }
+    }
+}
